6d8b472233779c2a028a03843603030d2b1aee86
Anthony PERARD <anthony.perard@citrix.com>
2012-05-21 16:12:43 +0000

    xen: Fix PV-on-HVM
    In the context of PV-on-HVM under Xen, the emulated nics are supposed to be
    unplug before the guest drivers are initialized, when the guest write to a
    specific IO port.
    
    Without this patch, the guest end up with two nics with the same MAC, the
    emulated nic and the PV nic.
    
    Signed-off-by: Anthony PERARD <anthony.perard@citrix.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- hw/xen_platform.c
+++ hw/xen_platform.c
@@ -87,7 +87,7 @@ static void unplug_nic(PCIBus *b, PCIDevice *d)
 {
     if (pci_get_word(d->config + PCI_CLASS_DEVICE) ==
             PCI_CLASS_NETWORK_ETHERNET) {
-        qdev_unplug(&(d->qdev));
+        qdev_free(&(d->qdev));
     }
 }
 
