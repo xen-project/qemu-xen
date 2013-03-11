f3903bbac78a81fcbce1350cdce860764a62783a
Alex Bligh <alex@alex.org.uk>
2013-03-11 14:02:49 +0000

    xen: Disable use of O_DIRECT by default as it results in crashes.
    Due to what is almost certainly a kernel bug, writes with O_DIRECT may
    continue to reference the page after the write has been marked as
    completed, particularly in the case of TCP retransmit. In other
    scenarios, this "merely" risks data corruption on the write, but with
    Xen pages from domU are only transiently mapped into dom0's memory,
    resulting in kernel panics when they are subsequently accessed.
    
    This brings PV devices in line with emulated devices.  Removing
    O_DIRECT is safe as barrier operations are now correctly passed
    through.
    
    See:
      http://lists.xen.org/archives/html/xen-devel/2012-12/msg01154.html
    for more details.
    
    Signed-off-by: Alex Bligh <alex@alex.org.uk>
    Acked-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- hw/xen_disk.c
+++ hw/xen_disk.c
@@ -744,7 +744,7 @@ static int blk_init(struct XenDevice *xendev)
     }
 
     /* read-only ? */
-    qflags = BDRV_O_NOCACHE | BDRV_O_CACHE_WB | BDRV_O_NATIVE_AIO;
+    qflags = BDRV_O_CACHE_WB | BDRV_O_NATIVE_AIO;
     if (strcmp(blkdev->mode, "w") == 0) {
         qflags |= BDRV_O_RDWR;
     } else {
