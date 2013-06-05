350cf49ff191f3270ec971e203cad3347d2931d5
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2013-06-05 11:33:02 +0000

    xen: simplify xen_enabled
    No need for preprocessor conditionals in xen_enabled: xen_allowed is
    always defined.
    
    upstream-commit-id: 49fa9881b2358e390e9e9466ddde74e995927efa
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- hw/xen.h
+++ hw/xen.h
@@ -25,11 +25,7 @@ extern int xen_allowed;
 
 static inline int xen_enabled(void)
 {
-#if defined(CONFIG_XEN_BACKEND) && !defined(CONFIG_NO_XEN)
     return xen_allowed;
-#else
-    return 0;
-#endif
 }
 
 int xen_pci_slot_get_pirq(PCIDevice *pci_dev, int irq_num);
