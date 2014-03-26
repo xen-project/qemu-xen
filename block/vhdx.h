e86cc068c2206a08b4552c513b0e3290ed681dd1
Jeff Cody <jcody@redhat.com>
2014-03-26 13:05:39 +0100

    vhdx: Bounds checking for block_size and logical_sector_size (CVE-2014-0148)
    Other variables (e.g. sectors_per_block) are calculated using these
    variables, and if not range-checked illegal values could be obtained
    causing infinite loops and other potential issues when calculating
    BAT entries.
    
    The 1.00 VHDX spec requires BlockSize to be min 1MB, max 256MB.
    LogicalSectorSize is required to be either 512 or 4096 bytes.
    
    Reported-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Jeff Cody <jcody@redhat.com>
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Reviewed-by: Max Reitz <mreitz@redhat.com>
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- block/vhdx.h
+++ block/vhdx.h
@@ -18,6 +18,11 @@
 #ifndef BLOCK_VHDX_H
 #define BLOCK_VHDX_H
 
+#define KiB              (1 * 1024)
+#define MiB            (KiB * 1024)
+#define GiB            (MiB * 1024)
+#define TiB ((uint64_t) GiB * 1024)
+
 /* Structures and fields present in the VHDX file */
 
 /* The header section has the following blocks,
@@ -262,6 +267,8 @@ typedef struct QEMU_PACKED VHDXMetadataTableEntry {
                                                    If set indicates a fixed
                                                    size VHDX file */
 #define VHDX_PARAMS_HAS_PARENT           0x02    /* has parent / backing file */
+#define VHDX_BLOCK_SIZE_MIN             (1   * MiB)
+#define VHDX_BLOCK_SIZE_MAX             (256 * MiB)
 typedef struct QEMU_PACKED VHDXFileParameters {
     uint32_t    block_size;             /* size of each payload block, always
                                            power of 2, <= 256MB and >= 1MB. */
