8820cc3906ac8fe3438e13d0ea66f4918c1286ba
Kevin Wolf <kwolf@redhat.com>
2015-03-05 11:02:45 +0000

    qcow1: Validate image size (CVE-2014-0223)
    A huge image size could cause s->l1_size to overflow. Make sure that
    images never require a L1 table larger than what fits in s->l1_size.
    
    This cannot only cause unbounded allocations, but also the allocation of
    a too small L1 table, resulting in out-of-bounds array accesses (both
    reads and writes).
    
    Cc: qemu-stable@nongnu.org
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- block/qcow.c
+++ block/qcow.c
@@ -60,7 +60,7 @@ typedef struct BDRVQcowState {
     int cluster_sectors;
     int l2_bits;
     int l2_size;
-    int l1_size;
+    unsigned int l1_size;
     uint64_t cluster_offset_mask;
     uint64_t l1_table_offset;
     uint64_t *l1_table;
@@ -135,7 +135,15 @@ static int qcow_open(BlockDriverState *bs, int flags)
 
     /* read the level 1 table */
     shift = s->cluster_bits + s->l2_bits;
-    s->l1_size = (header.size + (1LL << shift) - 1) >> shift;
+    if (header.size > UINT64_MAX - (1LL << shift)) {
+        goto fail;
+    } else {
+        uint64_t l1_size = (header.size + (1LL << shift) - 1) >> shift;
+        if (l1_size > INT_MAX / sizeof(uint64_t)) {
+            goto fail;
+        }
+        s->l1_size = l1_size;
+    }
 
     s->l1_table_offset = header.l1_table_offset;
     s->l1_table = g_malloc(s->l1_size * sizeof(uint64_t));
