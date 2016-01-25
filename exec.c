2600664a4e8d333290f01f0a957ea996e300bc6f
Prasad J Pandit <pjp@fedoraproject.org>
2016-01-25 19:59:50 +0530

    exec: fix a glitch in checking dma r/w access
    While checking r/w access in 'memory_access_is_direct' routine
    a glitch in the expression leads to segmentation fault while
    performing dma read operation.
    
    Reported-by: Donghai Zdh <donghai.zdh@alibaba-inc.com>
    Signed-off-by: Prasad J Pandit <pjp@fedoraproject.org>

--- exec.c
+++ exec.c
@@ -1903,7 +1903,7 @@ static void invalidate_and_set_dirty(hwaddr addr,
 static inline bool memory_access_is_direct(MemoryRegion *mr, bool is_write)
 {
     if (memory_region_is_ram(mr)) {
-        return !(is_write && mr->readonly);
+        return (is_write && !mr->readonly);
     }
     if (memory_region_is_romd(mr)) {
         return !is_write;
