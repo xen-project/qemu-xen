36d4ccc357252ff5506352c4815533f0a3ebc116
Stefano Stabellini <stefano.stabellini@eu.citrix.com>
2016-02-11 17:20:16 +0000

    Revert "exec: fix a glitch in checking dma r/w access"
    This reverts commit 47f168e2da96473ede608a17aa757c11bc90fc5f.
    The patch should not have been committed, see:
    http://marc.info/?l=qemu-devel&m=145373225020873.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@eu.citrix.com>

--- exec.c
+++ exec.c
@@ -347,7 +347,7 @@ address_space_translate_internal(AddressSpaceDispatch *d, hwaddr addr, hwaddr *x
 static inline bool memory_access_is_direct(MemoryRegion *mr, bool is_write)
 {
     if (memory_region_is_ram(mr)) {
-        return (is_write && !mr->readonly);
+        return !(is_write && mr->readonly);
     }
     if (memory_region_is_romd(mr)) {
         return !is_write;
